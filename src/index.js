663c9055dfdf4409314439974df678509bc82979
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-06-10 00:55:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+
+import './css/styles.css';
 import Notiflix from 'notiflix';
+import { fetchCatByBreed, fetchBreeds } from './cat-api';
+
+const breedSelectRef = document.querySelector('.breed-select');
+const catInfoRef = document.querySelector('.cat-info');
+const loaderRef = document.querySelector('.loader');
+
+loaderRef.classList.add('hidden');
+catInfoRef.classList.add('hidden');
+let breedId = '';
